56f623f7e7a908af40e9871d5482919c3f806081
alireza kargar <alirezakargarsuport@gmail.com>
2024-02-17 17:48:36 +0330

    upload

---
+++ src/sections/home/view/home-view.tsx
@@ -0,0 +1,128 @@
+'use client';
+
+import { useScroll } from 'framer-motion';
+
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+
+import MainLayout from 'src/layouts/main';
+
+import ScrollProgress from 'src/components/scroll-progress';
+
+import Image from 'src/components/image';
+import HomeHero from '../home-hero';
+import HomeMinimal from '../home-minimal';
+import HomePricing from '../home-pricing';
+import HomeDarkMode from '../home-dark-mode';
+import HomeLookingFor from '../home-looking-for';
+import HomeForDesigner from '../home-for-designer';
+import HomeColorPresets from '../home-color-presets';
+import HomeAdvertisement from '../home-advertisement';
+import HomeCleanInterfaces from '../home-clean-interfaces';
+import HomeHugePackElements from '../home-hugepack-elements';
+import { Card, CardContent, CardHeader, Container, Stack } from '@mui/material';
+import { _carouselsExample } from 'src/sections/_examples/extra/carousel-view';
+import CarouselBasic3 from 'src/sections/_examples/extra/carousel-view/carousel-basic-3';
+import CarouselBasic1 from 'src/sections/_examples/extra/carousel-view/carousel-basic-1';
+import { HEADER } from 'src/layouts/config-layout';
+import { useCallback, useEffect, useState } from 'react';
+
+// ----------------------------------------------------------------------
+
+type StyledPolygonProps = {
+  anchor?: 'top' | 'bottom';
+};
+
+const StyledPolygon = styled('div')<StyledPolygonProps>(({ anchor = 'top', theme }) => ({
+  left: 0,
+  zIndex: 9,
+  height: 80,
+  width: '100%',
+  position: 'absolute',
+  clipPath: 'polygon(0% 0%, 100% 100%, 0% 100%)',
+  backgroundColor: theme.palette.background.default,
+  display: 'block',
+  lineHeight: 0,
+  ...(anchor === 'top' && {
+    top: -1,
+    transform: 'scale(-1, -1)',
+  }),
+  ...(anchor === 'bottom' && {
+    bottom: -1,
+    backgroundColor: theme.palette.grey[900],
+  }),
+}));
+
+// ----------------------------------------------------------------------
+
+export default function HomeView() {
+
+  console.log(HEADER)
+
+  const { scrollY, scrollYProgress } = useScroll();
+
+  const [percent, setPercent] = useState(0);
+
+  const getScroll = useCallback(() => {
+    let heroHeight = 0;
+
+    // if (heroRef.current) {
+    //   heroHeight = heroRef.current.offsetHeight;
+    // }
+
+    scrollY.on('change', (scrollHeight) => {
+      const scrollPercent = (scrollHeight * 100) / heroHeight;
+
+      setPercent(Math.floor(scrollPercent));
+    });
+  }, [scrollY]);
+
+  useEffect(() => {
+    getScroll();
+  }, [getScroll]);
+
+  return (
+    <MainLayout>
+      <ScrollProgress scrollYProgress={scrollYProgress} />
+
+      {/* <HomeHero /> */}
+
+      <Box
+        sx={{
+          overflow: 'hidden',
+          position: 'relative',
+          bgcolor: 'background.default',
+        }}
+      >
+        {/* <HomeMinimal /> */}
+
+        <Container maxWidth={'xl'} sx={{ mt: `${HEADER.H_DESKTOP}px` }}>
+          <Stack spacing={10}>
+            <CarouselBasic1 data={_carouselsExample.slice(0, 8)} />
+            <Image alt={'karano'} src={'/assets/icons/product/wood_karano.jpg'} sx={{ width: 1 }} />
+          </Stack>
+        </Container>
+
+        <HomeHugePackElements />
+
+        <Box sx={{ position: 'relative' }}>
+          <StyledPolygon />
+          <HomeForDesigner />
+          <StyledPolygon anchor="bottom" />
+        </Box>
+
+        <HomeDarkMode />
+
+        <HomeColorPresets />
+
+        <HomeCleanInterfaces />
+
+        <HomePricing />
+
+        <HomeLookingFor />
+
+        <HomeAdvertisement />
+      </Box>
+    </MainLayout>
+  );
+}
---
+++ src/sections/product/product-slider-item.tsx
@@ -0,0 +1,61 @@
+import { Stack } from "@mui/material";
+import { IProductItem } from "src/types/product";
+import Image from 'src/components/image';
+import { Box, Button, Divider, Grid, IconButton, Typography } from '@mui/material';
+import Iconify from 'src/components/iconify';
+import { useState } from "react";
+import { varFade, varScale, MotionContainer, MotionViewport } from 'src/components/animate';
+
+type Props = {
+    product?: IProductItem;
+    ind: number
+};
+
+export default function ProductItemSlider({ product, ind }: Props) {
+    console.log(ind)
+    const [img, setImg] = useState('/img/product/product.png')
+    const [hover, setHover] = useState(false)
+
+    const onHoverHandler = () => {
+        setImg('/img/product/product-hover.png')
+        setHover(true)
+    }
+
+    const onHoverOutHandler = () => {
+        setImg('/img/product/product.png')
+        setHover(false)
+    }
+
+    const Img = (props: any) => (
+        <Image
+            component={MotionViewport}
+            key={props.key}
+            src={img}
+            {...varFade({ durationIn: 1, durationOut: 0.25 }).in}
+            ratio="1/1"
+            border={'1px solid #E0E0E0'}
+            borderRadius={'8px'}
+            onMouseOver={onHoverHandler}
+            onMouseOut={onHoverOutHandler}
+        />
+    )
+
+    return (
+        <Stack sx={{ textAlign: 'left', alignItems: 'end' }} spacing={1} key={ind} >
+            {hover ? <Img key={1} /> : <Img key={2} /> }
+
+
+            <Typography variant='h4' sx={{
+                mt: 1, '&:hover': {
+                    cursor: 'pointer'
+                }
+            }}>درب ضد سرقت</Typography>
+            <Stack direction={'row'}>
+                <Typography variant='subtitle2' sx={{ pt: 0.5, pl: 1 }}>کد 65</Typography>
+                <IconButton size='medium' sx={{ p: 0 }}>
+                    <Iconify icon="icon-park-solid:like" />
+                </IconButton>
+            </Stack>
+        </Stack>
+    )
+}
