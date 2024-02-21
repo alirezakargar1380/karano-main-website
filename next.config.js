1ff7d2620e02d488c67ff4b24312eae3af368fe5
alireza kargar <alirezakargarsuport@gmail.com>
2024-02-21 21:19:57 +0330

    update

--- next.config.js
+++ next.config.js
@@ -15,4 +15,10 @@ module.exports = {
     });
     return config;
   },
+  typescript: {
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
 };
