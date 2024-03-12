a3da0e89e4f6ba4d6140be47de6c44754cbfdff0
alireza kargar <alirezakargarsuport@gmail.com>
2024-03-12 21:15:23 +0330

    Update order-tracking-view.tsx

--- src/sections/order-tracking/view/order-tracking-view.tsx
+++ src/sections/order-tracking/view/order-tracking-view.tsx
@@ -76,6 +76,144 @@ export default function OrderTrackingView() {
                         </Stack>
                     </Stack>
                 </Box>
+                <Box sx={{ bgcolor: '#F8F8F8', borderRadius: '16px', border: '1px solid #D1D1D1', padding: 4 }}>
+                    <Stack direction={'row'} justifyContent={'space-between'} sx={{ borderBottom: '1px solid #D1D1D1', pb: 2 }}>
+                        <Label color="success" fontFamily={'peyda-bold'}>وضعیت: تایید شده</Label>
+                        <StyledRoundedWhiteButton
+                            variant="outlined">
+                            مشاهده پیش فاکتور
+                            <Iconify icon={'solar:arrow-left-linear'} sx={{ ml: 1 }} />
+                        </StyledRoundedWhiteButton>
+                    </Stack>
+                    <Stack direction={'row'} justifyContent={'space-between'} sx={{ pt: 2 }}>
+                        <Stack spacing={2} sx={{ width: 0.5 }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>شماره سفارش:</Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    15558476
+                                </Typography>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                                    تاریخ ثبت سفارش:
+                                </Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    1402/02/01
+                                </Typography>
+                            </Box>
+                        </Stack>
+                        <Stack spacing={2} sx={{ width: 0.5 }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                                    تعداد کالا:
+                                </Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    59
+                                </Typography>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                                    تاریخ تحویل:
+                                </Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    1402/02/01
+                                </Typography>
+                            </Box>
+                        </Stack>
+                    </Stack>
+                </Box>
+                <Box sx={{ bgcolor: '#F8F8F8', borderRadius: '16px', border: '1px solid #D1D1D1', padding: 4 }}>
+                    <Stack direction={'row'} justifyContent={'space-between'} sx={{ borderBottom: '1px solid #D1D1D1', pb: 2 }}>
+                        <Label color="success" fontFamily={'peyda-bold'}>وضعیت: تایید شده</Label>
+                        <StyledRoundedWhiteButton
+                            variant="outlined">
+                            مشاهده پیش فاکتور
+                            <Iconify icon={'solar:arrow-left-linear'} sx={{ ml: 1 }} />
+                        </StyledRoundedWhiteButton>
+                    </Stack>
+                    <Stack direction={'row'} justifyContent={'space-between'} sx={{ pt: 2 }}>
+                        <Stack spacing={2} sx={{ width: 0.5 }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>شماره سفارش:</Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    15558476
+                                </Typography>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                                    تاریخ ثبت سفارش:
+                                </Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    1402/02/01
+                                </Typography>
+                            </Box>
+                        </Stack>
+                        <Stack spacing={2} sx={{ width: 0.5 }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                                    تعداد کالا:
+                                </Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    59
+                                </Typography>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                                    تاریخ تحویل:
+                                </Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    1402/02/01
+                                </Typography>
+                            </Box>
+                        </Stack>
+                    </Stack>
+                </Box>
+                <Box sx={{ bgcolor: '#F8F8F8', borderRadius: '16px', border: '1px solid #D1D1D1', padding: 4 }}>
+                    <Stack direction={'row'} justifyContent={'space-between'} sx={{ borderBottom: '1px solid #D1D1D1', pb: 2 }}>
+                        <Label color="success" fontFamily={'peyda-bold'}>وضعیت: تایید شده</Label>
+                        <StyledRoundedWhiteButton
+                            variant="outlined">
+                            مشاهده پیش فاکتور
+                            <Iconify icon={'solar:arrow-left-linear'} sx={{ ml: 1 }} />
+                        </StyledRoundedWhiteButton>
+                    </Stack>
+                    <Stack direction={'row'} justifyContent={'space-between'} sx={{ pt: 2 }}>
+                        <Stack spacing={2} sx={{ width: 0.5 }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>شماره سفارش:</Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    15558476
+                                </Typography>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                                    تاریخ ثبت سفارش:
+                                </Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    1402/02/01
+                                </Typography>
+                            </Box>
+                        </Stack>
+                        <Stack spacing={2} sx={{ width: 0.5 }}>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                                    تعداد کالا:
+                                </Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    59
+                                </Typography>
+                            </Box>
+                            <Box sx={{ display: 'flex', flexDirection: 'row' }}>
+                                <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                                    تاریخ تحویل:
+                                </Typography>
+                                <Typography fontFamily={'peyda-medium'}>
+                                    1402/02/01
+                                </Typography>
+                            </Box>
+                        </Stack>
+                    </Stack>
+                </Box>
             </Stack>
         </Box>
     )
