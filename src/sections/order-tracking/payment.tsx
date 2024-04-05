4aa09b60595ce08e42ebd88b9b228b08c7fbc236
alireza kargar <alirezakargarsuport@gmail.com>
2024-04-05 16:34:44 +0330

    Update payment.tsx

--- src/sections/order-tracking/payment.tsx
+++ src/sections/order-tracking/payment.tsx
@@ -1,17 +1,41 @@
-import { Box} from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import { useCheckoutContext } from "../checkout/context";
 import { Stack } from "@mui/system";
 import { StyledRoundedWhiteButton } from "src/components/styles/props/rounded-white-button";
 import { LoadingButton } from "@mui/lab";
 import { IInvoice } from "src/types/invoice";
+import { RHFTitleTextField } from "src/components/hook-form";
 
 
 
 export default function Payment() {
     const checkout = useCheckoutContext();
-      
+
     return (
         <Box>
+            <Typography variant="h4" sx={{ width: 1, pb: 2, fontFamily: 'peyda-bold', borderBottom: '1px solid #D1D1D1' }}>
+                پیش پرداخت
+            </Typography>
+            <Box sx={{ border: '2px solid #A9A9A9', borderRadius: '16px', p: 4 }}>
+                <Typography variant="h4" sx={{ width: 1, pb: 2, fontFamily: 'peyda-bold', borderBottom: '1px solid #D1D1D1' }}>
+                    مشخصات صاحب فاکتور
+                </Typography>
+                <Typography variant="body2" sx={{ width: 1, py: 2, fontFamily: 'peyda-regular' }}>
+                    در صورت داشتن کد تخفیف، آن را وارد کنید.
+                </Typography>
+                <Stack
+                    direction={{ xs: 'column', sm: 'row' }}
+                    display="grid"
+                    gridTemplateColumns={{
+                        xs: 'repeat(1, 1fr)',
+                        md: 'repeat(2, 1fr)',
+                    }}
+                    sx={{ mt: 2 }}
+                    spacing={2}
+                >
+                    <RHFTitleTextField name='first_name' custom_label='کد تخفیف' placeholder='666784' />
+                </Stack>
+            </Box>
             <Stack sx={{ mt: 2 }} direction={'row'} spacing={1} justifyContent={'end'}>
                 <StyledRoundedWhiteButton variant='outlined' sx={{ px: 4 }} onClick={() => checkout.onBackStep()}>مرحله قبل</StyledRoundedWhiteButton>
                 <LoadingButton variant='contained' sx={{ borderRadius: '24px', px: 4 }} onClick={() => checkout.onNextStep()}>ثبت و ادامه</LoadingButton>
