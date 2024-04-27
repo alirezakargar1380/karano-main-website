ecb8384df3fb9111b417ccbd7bfba55032b8f1dd
alireza kargar <alirezakargarsuport@gmail.com>
2024-04-27 09:38:11 +0330

    Update page.tsx

--- src/app/admin-dashboard/sale-management/[id]/page.tsx
+++ src/app/admin-dashboard/sale-management/[id]/page.tsx
@@ -21,10 +21,10 @@ export default function SaleManagementDetailsPage({ params }: Props) {
   return <SaleManagementDetailsView id={id} />;
 }
 
-export async function generateStaticParams() {
-  const res = await axios.get(endpoints.product.list);
+// export async function generateStaticParams() {
+//   const res = await axios.get(endpoints.product.list);
 
-  return res.data.products.map((product: { id: string }) => ({
-    id: product.id,
-  }));
-}
+//   return res.data.products.map((product: { id: string }) => ({
+//     id: product.id,
+//   }));
+// }
