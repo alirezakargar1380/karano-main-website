26002ae93567ed7c474f4cd985e43650255e7839
alireza kargar <alirezakargarsuport@gmail.com>
2024-07-15 04:04:08 +0330

    add product to favorite

--- src/sections/product/product-slider-item.tsx
+++ src/sections/product/product-slider-item.tsx
@@ -3,12 +3,14 @@ import { IProductItem } from "src/types/product";
 import Image from 'src/components/image';
 import { Box, Button, Divider, Grid, IconButton, Typography } from '@mui/material';
 import Iconify from 'src/components/iconify';
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import { varFade, varScale, MotionContainer, MotionViewport, varZoom } from 'src/components/animate';
 import { m } from 'framer-motion';
 import TiltCard from "src/components/animation/tilt-card";
 import SvgColor from "src/components/svg-color";
-import { endpoints } from "src/utils/axios";
+import { endpoints, server_axios } from "src/utils/axios";
+import { useSnackbar } from 'src/components/snackbar';
+import { useAuthContext } from "src/auth/hooks";
 
 type Props = {
     product: IProductItem;
@@ -16,6 +18,9 @@ type Props = {
 };
 
 export default function ProductItemSlider({ product, ind }: Props) {
+    const { authenticated } = useAuthContext();
+    const [isFavorite, setFavorite] = useState<boolean>(false);
+
     const mainImageUrl = endpoints.image.url(product?.images?.find((img) => img.main)?.name || '');
     const hoverImageUrl = endpoints.image.url(product?.images?.find((img) => !img.main)?.name || '');
 
@@ -32,7 +37,25 @@ export default function ProductItemSlider({ product, ind }: Props) {
         setHover(false)
     }
 
-    // console.log(ind)
+    const { enqueueSnackbar } = useSnackbar();
+
+    const handleAddToFavorites = useCallback(async () => {
+        await server_axios.post(endpoints.favorite.create, {
+            product: { id: product.id },
+        })
+        setFavorite(true);
+        enqueueSnackbar("محصول به علاقه مندی های شما اضافه شد", {
+            variant: 'info'
+        })
+    }, [product]);
+
+    const handleRemoveToFavorites = useCallback(async () => {
+        await server_axios.delete(endpoints.favorite.delete(product.id))
+        setFavorite(false);
+        enqueueSnackbar("محصول از علاقه مندی های شما حذف شد", {
+            variant: 'error'
+        });
+    }, [product]);
 
     const Img = () => {
         return (
@@ -56,34 +79,36 @@ export default function ProductItemSlider({ product, ind }: Props) {
     }
 
     return (
-        <Link href={`/product/${product?.id}/`} color={'inherit'} underline="none" key={ind}>
-            {/* <TiltCard> */}
-                <Box sx={{
-                    transform: !hover ? 'scale(0.98)' : 'scale(1)',
-                    transition: '0.3s ease-in-out'
-                }}
-                >
-                    <Stack sx={{ textAlign: 'right', alignItems: 'start' }} spacing={1}>
-                        {hover ? <Img /> : <Img />}
+        <Box sx={{
+            transform: !hover ? 'scale(0.98)' : 'scale(1)',
+            transition: '0.3s ease-in-out'
+        }}
+        >
+            <Link href={`/product/${product?.id}/`} color={'inherit'} underline="none" key={ind}>
+                <Stack sx={{ textAlign: 'right', alignItems: 'start' }} spacing={1}>
+                    {hover ? <Img /> : <Img />}
 
-                        <Typography variant='h5' sx={{
-                            mt: 1, '&:hover': {
-                                cursor: 'pointer'
-                            },
-                            textAlign: 'left'
-                        }}>
-                            {product?.name}
-                        </Typography>
-                        <Stack direction={'row-reverse'}>
-                            <Typography sx={{ pt: 0.5, pl: 1, fontSize: '16px' }} fontFamily={'peyda-regular'}>کد {product?.code}</Typography>
-                            <IconButton size='small' sx={{ bgcolor: "#D1D1D1" }}>
-                                {/* <Iconify icon="icon-park-solid:like" /> */}
-                                <SvgColor src="/assets/icons/product/save-icon-products.svg" color={"#fff"} sx={{ width: 20, height: 20 }} />
-                            </IconButton>
-                        </Stack>
-                    </Stack>
-                </Box>
-            {/* </TiltCard> */}
-        </Link>
+                    <Typography variant='h5' sx={{
+                        mt: 1, '&:hover': {
+                            cursor: 'pointer'
+                        },
+                        textAlign: 'left'
+                    }}>
+                        {product?.name}
+                    </Typography>
+
+                </Stack>
+            </Link>
+            <Stack direction={'row'} sx={{ textAlign: 'left', alignItems: 'end' }}>
+                <IconButton size='small' sx={{ bgcolor: "#D1D1D1" }} disabled={!authenticated} onClick={isFavorite ? handleRemoveToFavorites : handleAddToFavorites}>
+                    {isFavorite ?
+                        <SvgColor src="/assets/icons/product/saved-icon-products.svg" color={"#000"} sx={{ width: 20, height: 20 }} />
+                        :
+                        <SvgColor src="/assets/icons/product/save-icon-products.svg" color={"#fff"} sx={{ width: 20, height: 20 }} />
+                    }
+                </IconButton>
+                <Typography sx={{ pt: 0.5, pl: 1, fontSize: '16px' }} fontFamily={'peyda-regular'}>کد {product?.code}</Typography>
+            </Stack>
+        </Box>
     )
 }
