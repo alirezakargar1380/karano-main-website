de5b3bd27e3707f8a12325851ac281e9e77db3b7
alireza kargar <alirezakargarsuport@gmail.com>
2024-07-17 19:27:34 +0330

    add edit company user in user panel

--- src/sections/user-panel/views/user-details-view.tsx
+++ src/sections/user-panel/views/user-details-view.tsx
@@ -2,43 +2,79 @@ import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import UserDetailsRow from "../user-details-row";
 import { useAuthContext } from "src/auth/hooks";
+import { IUserTypes } from "src/types/user";
 
 export default function UserDetailsView() {
     const { user, authenticated } = useAuthContext();
-    console.log(user);
     return (authenticated && user) && (
-        <Box sx={{
-        }}>
-            <UserDetailsRow
-                name="user_type"
-                title="نوع کاربر"
-                value={user.user_type}
-            />
-            <UserDetailsRow
-                name="first_name"
-                title="نام"
-                value={user.first_name}
-            />
-            <UserDetailsRow
-                name="last_name"
-                title="نام خانوادگی"
-                value={user.last_name}
-            />
-            <UserDetailsRow
-                name="id_code"
-                title="کد ملی"
-                value={user.id_code}
-            />
-            <UserDetailsRow
-                name="phone"
-                title="شماره موبایل"
-                value={user.phone}
-            />
-            <UserDetailsRow
-                name="email"
-                title="ایمیل"
-                value={user.email}
-            />
-        </Box>
+        <>
+            {(user.user_type === IUserTypes.genuine) ? (
+                <Box>
+                    <UserDetailsRow
+                        name="user_type"
+                        title="نوع کاربر"
+                        value={user.user_type}
+                    />
+                    <UserDetailsRow
+                        name="first_name"
+                        title="نام"
+                        value={user.first_name}
+                    />
+                    <UserDetailsRow
+                        name="last_name"
+                        title="نام خانوادگی"
+                        value={user.last_name}
+                    />
+                    <UserDetailsRow
+                        name="id_code"
+                        title="کد ملی"
+                        value={user.id_code}
+                    />
+                    <UserDetailsRow
+                        name="phone"
+                        title="شماره موبایل"
+                        value={user.phone}
+                    />
+                    <UserDetailsRow
+                        name="email"
+                        title="ایمیل"
+                        value={user.email}
+                    />
+                </Box>
+            ) : (
+                <Box>
+                    <UserDetailsRow
+                        name="user_type"
+                        title="نوع کاربر"
+                        value={user.user_type}
+                    />
+                    <UserDetailsRow
+                        name="company_name"
+                        title="نام شرکت"
+                        value={user.company_name}
+                    />
+                    <UserDetailsRow
+                        name="national_id"
+                        title="کد اقتصادی"
+                        value={user.national_id}
+                    />
+                    <UserDetailsRow
+                        name="landline_number"
+                        title="شماره تلفن ثابت"
+                        value={user.landline_number}
+                    />
+                    <UserDetailsRow
+                        name="phone"
+                        title="شماره موبایل"
+                        value={user.phone}
+                    />
+                    <UserDetailsRow
+                        name="email"
+                        title="ایمیل"
+                        value={user.email}
+                    />
+                </Box>
+            )}
+        </>
     )
 }
