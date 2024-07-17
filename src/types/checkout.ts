c19ba8abc96abf3fa3e78b9360036771c60f51c8
alireza kargar <alirezakargarsuport@gmail.com>
2024-07-17 19:29:04 +0330

    Update checkout.ts

--- src/types/checkout.ts
+++ src/types/checkout.ts
@@ -81,7 +81,7 @@ export type CheckoutContextProps = ICheckoutValue & {
   //
   onGetCart: () => void;
   onAddToCart: (newItem: Partial<ICheckoutNewItem>, concatWithProperty: boolean) => void;
-  onDeleteCart: (index: number, itemIndex: number, propertyIndex: number) => void;
+  onDeleteCart: (pId: number, itemIndex: number, propertyIndex: number) => void;
   //
   onIncreaseQuantity: (itemId: string) => void;
   onDecreaseQuantity: (itemId: string) => void;
