d629e23eed7fb86d4fdd0364c559581490ad8c1f
alireza kargar <alirezakargarsuport@gmail.com>
2024-07-24 13:40:12 +0330

    Update page.tsx

--- src/app/shopping-cart/page.tsx
+++ src/app/shopping-cart/page.tsx
@@ -1,6 +1,9 @@
 import { ShoppingCartView } from 'src/sections/shopping-cart/view';
 
 // ----------------------------------------------------------------------
+export const metadata = {
+  title: 'cart',
+};
 
 export default function ShoppingCardPage() {
   return <ShoppingCartView />;
