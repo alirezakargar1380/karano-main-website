31d442af416489eb4b45b0cc4d8695ec2e2bb3e0
alireza kargar <alirezakargarsuport@gmail.com>
2024-08-29 04:48:22 +0330

    add show one time hook

---
+++ src/hooks/use-show-one-time.ts
@@ -0,0 +1,16 @@
+import { useMemo } from "react";
+import { useLocalStorage } from "src/hooks/use-local-storage";
+
+export function useShowOneTime(section_name: string) {
+    const { state, update } = useLocalStorage(section_name, "0");
+
+    const memoizedValue = useMemo(
+        () => ({
+            show: state[section_name] == 1 ? true : false,
+            toggle: () => update(section_name, state[section_name] == "1" ? "0" : "1")
+        }),
+        [state[section_name], update]
+    );
+
+    return memoizedValue;
+}
