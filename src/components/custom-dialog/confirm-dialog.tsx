f59f4948eba702e72e3c28d0aecae398006f5d9f
alireza kargar <alirezakargarsuport@gmail.com>
2024-10-20 14:11:39 +0330

    fix width

--- src/components/custom-dialog/confirm-dialog.tsx
+++ src/components/custom-dialog/confirm-dialog.tsx
@@ -20,20 +20,27 @@ export default function ConfirmDialog({
   ...other
 }: ConfirmDialogProps) {
   return (
-    <Dialog fullWidth maxWidth="xs" open={open} onClose={onClose} {...other} sx={{
+    <Dialog open={open} maxWidth={false} onClose={onClose} {...other} sx={{
       '& .MuiBackdrop-root': {
         backgroundColor: 'rgba(0,0,0,0.8)'
+      },
+      '& .MuiPaper-root': {
+        width: '640px'
       }
     }}>
-      <Box py={1} px={3}>
+      <Box py={2} px={4} width={1}>
         <DialogTitle sx={{ pb: 2, borderBottom: '1px solid #E8E8E8', pt: 2, display: 'flex', alignItems: 'center', fontFamily: 'peyda-bold', px: 0 }}>
           <SvgColor src="/assets/icons/notification/alert-circle.svg" color={"#D12215"} mr={1} />
-          <Box>
+          <Box sx={{ fontFamily: 'peyda-bold', fontSize: 22 }}>
             {title}
           </Box>
         </DialogTitle>
 
-        {content && <Box sx={{ typography: 'body2', pt: 2 }}> {content} </Box>}
+        {content &&
+          <Box sx={{ fontSize: 16, fontFamily: 'peyda-bold', fontWeight: 600, lineHeight: '32px', pt: 3, whiteSpace: 'pre-line', color: '#727272' }}>
+            {content}
+          </Box>
+        }
 
         <DialogActions sx={{ px: 0 }}>
           <Button color="inherit" onClick={onClose}>
