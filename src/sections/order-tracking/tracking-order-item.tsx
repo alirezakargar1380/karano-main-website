7496165bb53ce8a3ea594572ceca9494fe222d6e
alireza kargar <alirezakargarsuport@gmail.com>
2024-10-21 03:54:24 +0330

    UI: order-tracking

--- src/sections/order-tracking/tracking-order-item.tsx
+++ src/sections/order-tracking/tracking-order-item.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Stack, Typography } from "@mui/material"
+import { Box, Button, Stack, SvgIcon, Typography } from "@mui/material"
 import { useCallback } from "react"
 import Iconify from "src/components/iconify"
 import Label from "src/components/label"
@@ -8,6 +8,8 @@ import { IOrderItem, OrderStatus } from "src/types/order"
 import { fDateTime, fToJamali } from "src/utils/format-time"
 
 import { useSnackbar } from 'src/components/snackbar';
+import { toFarsiNumber } from "src/utils/change-case"
+import { pxToRem } from "src/theme/typography"
 
 interface Props {
     order: IOrderItem
@@ -26,44 +28,50 @@ export default function TrackingOrderItem({ order, handleMoreBtn }: Props) {
     }, []);
 
     return (
-        <Box sx={{ bgcolor: '#F8F8F8', borderRadius: '16px', border: '1px solid #D1D1D1', padding: 4 }}>
-            <Stack direction={'row'} justifyContent={'space-between'} sx={{ borderBottom: '1px solid #D1D1D1', pb: 2 }}>
-                <Label
-                    sx={{
-                        ...(order.status === OrderStatus.produced && {
-                            bgcolor: "#0B7BA7",
-                            color: "white"
-                        })
-                    }}
-                    color={
-                        ((order.status === OrderStatus.pending
-                            || order.status === OrderStatus.ready_to_send
-                            || order.status === OrderStatus.preparing
-                            || order.status === OrderStatus.production) && 'info') ||
-                        ((order.status === OrderStatus.failed || order.status === OrderStatus.removed)
-                            && 'error') ||
-                        ((
-                            order.status === OrderStatus.accepted ||
-                            order.status === OrderStatus.posted ||
-                            order.status === OrderStatus.ready_to_send
-                        ) && 'success') ||
-                        (order.status === OrderStatus.edited && 'warning') ||
-                        'default'
-                    } fontFamily={'peyda-bold'}>
-                    {' وضعیت: '}
-                    {
-                        (order.status === OrderStatus.posted && 'ارسال شده') ||
-                        (order.status === OrderStatus.edited && 'اصلاح شده') ||
-                        (order.status === OrderStatus.pending && 'در حال بررسی') ||
-                        (order.status === OrderStatus.failed && 'رد شده') ||
-                        (order.status === OrderStatus.production && 'در حال تولید') ||
-                        (order.status === OrderStatus.produced && 'در انتظار پرداخت نهایی') ||
-                        (order.status === OrderStatus.ready_to_send && 'آماده ارسال') ||
-                        (order.status === OrderStatus.accepted && 'تایید شده') ||
-                        (order.status === OrderStatus.removed && 'حذف شده') ||
-                        (order.status === OrderStatus.preparing && 'در حال آماده سازی')
-                    }
-                </Label>
+        <Box sx={{ bgcolor: '#F8F8F8', borderRadius: '16px', border: '1px solid #D1D1D1', px: pxToRem(24), pt: pxToRem(24) }}>
+            <Stack direction={'row'} justifyContent={'space-between'} sx={{ borderBottom: '1px solid #D1D1D1', pb: pxToRem(16) }}>
+                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                    <Label
+                        sx={{
+                            ...(order.status === OrderStatus.produced && {
+                                bgcolor: "#0B7BA7",
+                                color: "white"
+                            }),
+                            px: pxToRem(8),
+                        }}
+                        color={
+                            ((order.status === OrderStatus.pending
+                                || order.status === OrderStatus.ready_to_send
+                                || order.status === OrderStatus.preparing
+                                || order.status === OrderStatus.production) && 'info') ||
+                            ((order.status === OrderStatus.failed || order.status === OrderStatus.removed)
+                                && 'error') ||
+                            ((
+                                order.status === OrderStatus.accepted ||
+                                order.status === OrderStatus.posted ||
+                                order.status === OrderStatus.ready_to_send
+                            ) && 'success') ||
+                            (order.status === OrderStatus.edited && 'warning') ||
+                            'default'
+                        } fontFamily={'peyda-bold'}>
+                        <Typography variant={'body3'}>
+                            {' وضعیت: '}
+                            {
+                                (order.status === OrderStatus.posted && 'ارسال شده') ||
+                                (order.status === OrderStatus.edited && 'اصلاح شده') ||
+                                (order.status === OrderStatus.pending && 'در حال بررسی') ||
+                                (order.status === OrderStatus.failed && 'رد شده') ||
+                                (order.status === OrderStatus.production && 'در حال تولید') ||
+                                (order.status === OrderStatus.produced && 'در انتظار پرداخت نهایی') ||
+                                (order.status === OrderStatus.ready_to_send && 'آماده ارسال') ||
+                                (order.status === OrderStatus.accepted && 'تایید شده') ||
+                                (order.status === OrderStatus.removed && 'حذف شده') ||
+                                (order.status === OrderStatus.preparing && 'در حال آماده سازی')
+                            }
+                        </Typography>
+                    </Label>
+                </Box>
+
                 <Stack direction={'row'} spacing={1}>
                     {order.status === OrderStatus.produced && (
                         <Button
@@ -80,55 +88,60 @@ export default function TrackingOrderItem({ order, handleMoreBtn }: Props) {
                         onClick={() => {
                             handleMoreBtn(+order.id, order.status)
                         }}
+                        sx={{
+                            py: pxToRem(4),
+                            px: pxToRem(16),
+                        }}
                     >
-                        {
-                            ((order.status === OrderStatus.pending
-                                || order.status === OrderStatus.edited
-                                || order.status === OrderStatus.ready_to_send
-                                || order.status === OrderStatus.production
-                                || order.status === OrderStatus.preparing
-                                || order.status === OrderStatus.posted) && 'مشاهده سبد خرید') ||
-                            ((order.status === OrderStatus.failed) && 'مشاهده جزئیات') ||
-                            ((order.status === OrderStatus.accepted) && 'ادامه و تکمیل خرید') ||
-                            (order.status === OrderStatus.produced && 'پرداخت نهایی') ||
-                            'مشاهده جزئیات'
-                        }
-                        <Iconify icon={'solar:arrow-left-linear'} sx={{ ml: 1 }} />
+                        <Typography variant="button1" sx={{ fontFamily: "peyda-bold" }}>
+                            {
+                                ((order.status === OrderStatus.pending
+                                    || order.status === OrderStatus.edited
+                                    || order.status === OrderStatus.ready_to_send
+                                    || order.status === OrderStatus.production
+                                    || order.status === OrderStatus.preparing
+                                    || order.status === OrderStatus.posted) && 'مشاهده سبد خرید') ||
+                                ((order.status === OrderStatus.failed) && 'مشاهده جزئیات') ||
+                                ((order.status === OrderStatus.accepted) && 'ادامه و تکمیل خرید') ||
+                                (order.status === OrderStatus.produced && 'پرداخت نهایی') ||
+                                'مشاهده جزئیات'
+                            }
+                        </Typography>
+                        <SvgColor src="/assets/icons/orders/arrow-left.svg" sx={{ ml: pxToRem(8) }} />
                     </StyledRoundedWhiteButton>
                 </Stack>
-
             </Stack>
-            <Stack direction={'row'} justifyContent={'space-between'} sx={{ pt: 2 }}>
+            <Stack direction={'row'} justifyContent={'space-between'} sx={{ py: pxToRem(24) }}>
                 <Stack spacing={2} sx={{ width: 0.5 }}>
                     <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                        <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>شماره سفارش:</Typography>
-                        <Typography fontFamily={'peyda-medium'}>
-                            {order.order_number}
+                        <Typography fontFamily={'peyda-bold'} variant="body3" sx={{ pr: 1 }}>شماره سفارش:</Typography>
+                        <Typography fontFamily={'peyda-medium'} variant="body4">
+                            {toFarsiNumber(order.order_number)}
                         </Typography>
                     </Box>
                     <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                        <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                        <Typography fontFamily={'peyda-bold'} variant="body3" sx={{ pr: 1 }}>
                             تاریخ ثبت سفارش:
                         </Typography>
-                        <Typography fontFamily={'peyda-light'} variant="body1">
+                        <Typography fontFamily={'peyda-medium'} variant="body4">
                             {fToJamali(order.createdAt)}
                         </Typography>
                     </Box>
                 </Stack>
                 <Stack spacing={2} sx={{ width: 0.5 }}>
                     <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                        <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                        <Typography fontFamily={'peyda-bold'} variant="body3" sx={{ pr: 1 }}>
                             تعداد کالا:
                         </Typography>
-                        <Typography fontFamily={'peyda-medium'}>
-                            59
+                        <Typography fontFamily={'peyda-medium'} variant="body4">
+                            {toFarsiNumber(59)}
                         </Typography>
                     </Box>
                     <Box sx={{ display: 'flex', flexDirection: 'row' }}>
-                        <Typography fontFamily={'peyda-bold'} sx={{ pr: 1 }}>
+                        <Typography fontFamily={'peyda-bold'} variant="body3" sx={{ pr: 1 }}>
                             تاریخ تحویل:
                         </Typography>
-                        <Typography fontFamily={'peyda-light'} variant="body1">
+                        <Typography fontFamily={'peyda-medium'} variant="body4">
                             {fToJamali(order.production_date)}
                         </Typography>
                     </Box>
