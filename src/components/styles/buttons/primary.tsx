8cb842a51ae157b1156e540777e69bbab6faa326
alireza kargar <alirezakargarsuport@gmail.com>
2024-10-27 06:45:34 +0330

    Update primary.tsx

--- src/components/styles/buttons/primary.tsx
+++ src/components/styles/buttons/primary.tsx
@@ -1,76 +1,85 @@
-import { Box, Button, ButtonProps } from "@mui/material";
-import { styled, SxProps } from "@mui/system";
-import React, { useState, useCallback } from "react";
-import { pxToRem } from "src/theme/typography";
+import { Box, Button, ButtonProps } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import React, { useState, useCallback, MouseEventHandler } from 'react';
+import { pxToRem } from 'src/theme/typography';
 import { m } from 'framer-motion';
-import SvgColor from "src/components/svg-color";
+import SvgColor from 'src/components/svg-color';
 
 export const PrimaryButtonStyle = styled(Button)<ButtonProps>(({ theme, size }) => {
-    const sizeSm = size === 'small';
-    const sizeMd = size === 'medium';
-    return {
-        backgroundColor: '#000',
-        color: '#fff',
-        borderRadius: '24px',
-        padding: `${pxToRem(4)} ${pxToRem(20)}`,
-        lineHeight: '0px',
-        ...(sizeSm && {
-            minWidth: '102px',
-            padding: `${pxToRem(4)} ${pxToRem(12)}`,
-        }),
-        ...(sizeMd && {
-            minWidth: '122px',
-            padding: `${pxToRem(4)} ${pxToRem(24)}`,
-        }),
-        '&:hover': {
-            backgroundColor: '#555555',
-        },
-        '&:active': {
-            outline: '2px solid #D1D1D1',
-        },
-        '&.Mui-disabled': {
-            backgroundColor: '#E0E0E0',
-            color: '#F8F8F8'
-        }
-    };
+  const sizeSm = size === 'small';
+  const sizeMd = size === 'medium';
+  return {
+    backgroundColor: '#000',
+    color: '#fff',
+    borderRadius: '24px',
+    padding: `${pxToRem(4)} ${pxToRem(20)}`,
+    lineHeight: '0px',
+    ...(sizeSm && {
+      minWidth: '102px',
+      padding: `${pxToRem(4)} ${pxToRem(12)}`,
+      height: '36px',
+      ...theme.typography.button2,
+    }),
+    ...(sizeMd && {
+      minWidth: '122px',
+      padding: `${pxToRem(4)} ${pxToRem(24)}`,
+      ...theme.typography.button1,
+    }),
+    '&:hover': {
+      backgroundColor: '#555555',
+    },
+    '&:active': {
+      outline: '2px solid #D1D1D1',
+    },
+    '&.Mui-disabled': {
+      backgroundColor: '#E0E0E0',
+      color: '#F8F8F8',
+    },
+  };
 });
 
 type Props = ButtonProps & {
-    onClick?: () => void;
-    children?: React.ReactNode;
-    isLoading?: boolean;
-    sx?: SxProps;
-}
+  onClick?: MouseEventHandler<HTMLElement>;
+  children?: React.ReactNode;
+  isLoading?: boolean;
+};
 
 export const PrimaryButton: React.FC<Props> = ({
-    onClick,
-    children,
-    isLoading = false,
-    ...other
+  onClick,
+  children,
+  isLoading = false,
+  ...other
 }) => {
-    const [loading, setLoading] = useState(isLoading);
+  const [loading, setLoading] = useState(isLoading);
 
-    const handleOnClick = useCallback(() => {
+  const handleOnClick: MouseEventHandler<HTMLElement> = useCallback(
+    async (event) => {
+      if (onClick) {
         setLoading(true);
-        onClick?.();
-    }, [onClick]);
+        try {
+          onClick(event);
+        } finally {
+          setLoading(false);
+        }
+      }
+    },
+    [onClick]
+  );
 
-    return (
-        <PrimaryButtonStyle
-            onClick={handleOnClick}
-            disabled={loading}
-            {...other}
+  return (
+    <PrimaryButtonStyle onClick={handleOnClick} disabled={loading} {...other}>
+      {loading ? (
+        <Box
+          component={m.div}
+          animate={{ rotate: 360 }}
+          transition={{ ease: 'linear', duration: 2, repeat: Infinity }}
+          sx={{ width: 24, height: 24, my: '8px' }}
         >
-            {loading ? (
-                <Box
-                    component={m.div}
-                    animate={{ rotate: 360 }}
-                    transition={{ ease: 'linear', duration: 2, repeat: Infinity }}
-                    sx={{ width: 24, height: 24, my: '8px' }}
-                >
-                    <SvgColor src="/assets/icons/buttons/loader-anim.svg" sx={{ width: 24, height: 24 }} />
-                </Box>
-            ) : children}
-        </PrimaryButtonStyle>
-    )
-}
+          <SvgColor src="/assets/icons/buttons/loader-anim.svg" sx={{ width: 24, height: 24 }} />
+        </Box>
+      ) : (
+        children
+      )}
+    </PrimaryButtonStyle>
+  );
+};
