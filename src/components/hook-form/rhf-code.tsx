8d592623542f68a8e2322a7145a44567149d93ed
alireza kargar <alirezakargarsuport@gmail.com>
2024-11-10 14:17:29 +0330

    Update rhf-code.tsx
    add error state

--- src/components/hook-form/rhf-code.tsx
+++ src/components/hook-form/rhf-code.tsx
@@ -2,7 +2,9 @@ import { Controller, useFormContext } from 'react-hook-form';
 import { MuiOtpInput, MuiOtpInputProps } from 'mui-one-time-password-input';
 
 import FormHelperText from '@mui/material/FormHelperText';
-import { Box } from '@mui/system';
+import { Box, Stack } from '@mui/system';
+import SvgColor from '../svg-color';
+import { Typography } from '@mui/material';
 
 // ----------------------------------------------------------------------
 
@@ -20,7 +22,14 @@ export default function RHFCode({ name, helperText, ...other }: RHFCodesProps) {
       control={control}
       render={({ field, fieldState: { error } }) => (
         <div>
-          <Box sx={{ border: '1px solid #D1D1D1', borderRadius: '8px', overflow: 'hidden' }}>
+          <Box sx={{
+            border: '1px solid #D1D1D1',
+            ...(error && {
+              border: '1px solid red',
+            }),
+            borderRadius: '8px',
+            overflow: 'hidden'
+          }}>
             <Box width={0.5} mx={'auto'}>
               <MuiOtpInput
                 {...field}
@@ -35,11 +44,12 @@ export default function RHFCode({ name, helperText, ...other }: RHFCodesProps) {
                       "& input": {
                         px: 0,
                         textAlign: 'center',
-                        width: 1
+                        width: 1,
+
                       },
                       px: 0,
                       "& fieldset": {
-                        border: 'none'
+                        border: 'none',
                       }
                     }
                   },
@@ -49,9 +59,16 @@ export default function RHFCode({ name, helperText, ...other }: RHFCodesProps) {
             </Box>
           </Box>
 
-          {(error || helperText) && (
-            <FormHelperText error={!!error}>
-              {helperText || error?.message}
+          {error && (
+            <Stack direction={'row'}>
+              <SvgColor src='/assets/icons/input/alert-circle.svg' color={'#C80303'} mt={1} />
+              <Typography variant='body2' ml={0.5} mt={1}>{error?.message}</Typography>
+            </Stack>
+          )}
+
+          {(helperText) && (
+            <FormHelperText>
+              {helperText}
             </FormHelperText>
           )}
         </div>
