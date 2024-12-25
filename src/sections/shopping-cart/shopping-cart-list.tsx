1a4b4680349ba49070a9fca4479a0a1157ac7a99
alireza kargar <alirezakargarsuport@gmail.com>
2024-12-26 02:12:49 +0330

    Update shopping-cart-list.tsx

--- src/sections/shopping-cart/shopping-cart-list.tsx
+++ src/sections/shopping-cart/shopping-cart-list.tsx
@@ -219,11 +219,11 @@ export default function ShoppingCartList({ items, type, isMini, afterUpdate, ord
                             </Stack>
                         </Grid>
                         {((type !== 'edit')) && (
-                            <Grid item sm={2} sx={{ pt: 0 }}>
+                            <Grid item sm={2} sx={{ pt: '0px!important' }}>
                                 <Image src={endpoints.image.url(item.product.images.find((img) => img.main)?.name || '')} sx={{ height: 120, width: 120, border: '1px solid #D1D1D1', borderRadius: '8px' }} />
                             </Grid>
                         )}
-                        <Grid item sm={(type !== 'edit') ? 10 : 12} sx={{ pt: 0 }}>
+                        <Grid item sm={(type !== 'edit') ? 10 : 12} sx={{ pt: '0px!important' }}>
                             <Scrollbar sx={{ maxHeight: 680 }}>
                                 <Table size={'medium'} sx={{ minWidth: 780 }}>
                                     <TableHeadCustom
