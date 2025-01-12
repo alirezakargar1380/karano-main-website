de0365bed1013fc9ebacc37168f4c36694421db0
alireza kargar <alirezakargarsuport@gmail.com>
2025-01-13 01:39:41 +0330

    Update price-list-view.tsx

--- src/sections/price-list/price-list-view.tsx
+++ src/sections/price-list/price-list-view.tsx
@@ -3,18 +3,21 @@
 import { useEffect, useState } from 'react';
 import { endpoints, server_axios } from 'src/utils/axios';
 import { useRouter, useSearchParams } from 'src/routes/hooks';
+import { RouterLink } from 'src/routes/components';
+import { BACKEND_API, HOST_API } from 'src/config-global';
+import { Button } from '@mui/material';
 
 export default function PriceListView() {
     const router = useRouter();
 
     const [isDownloading, setIsDownloading] = useState(false);
-    
+
     useEffect(() => {
         const downloadPriceList = async () => {
             if (isDownloading) return;
-            
+
             setIsDownloading(true);
-            
+
             try {
                 const response = await server_axios.get(endpoints.settings.price_list, {
                     responseType: 'blob'
@@ -23,14 +26,14 @@ export default function PriceListView() {
                 const url = window.URL.createObjectURL(
                     new Blob([response.data], { type: 'application/pdf' })
                 );
-                
+
                 const link = document.createElement('a');
                 link.href = url;
                 link.setAttribute('download', 'price-list.pdf');
                 link.click();
-                
+
                 window.URL.revokeObjectURL(url);
-                
+
             } catch (error) {
                 console.error('Download failed:', error);
             }
@@ -40,8 +43,11 @@ export default function PriceListView() {
     }, [isDownloading]);
 
     return (
-        <div>
+        <div style={{ textAlign: 'center' }}>
             <h1>در حال دانلود لیست قیمت کارانو...</h1>
+            <Button component={RouterLink} href={BACKEND_API + endpoints.settings.price_list} variant="soft" color='error'>
+                در صورت دانلود نشدن اینجا کلیک کنید
+            </Button>
         </div>
     );
 }
