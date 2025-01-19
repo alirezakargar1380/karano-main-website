7562c0ff4d01440deeec8d331a342119dbc594f4
alireza kargar <alirezakargarsuport@gmail.com>
2025-01-19 14:56:46 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
 import { Metadata } from 'next';
+import { BACKEND_API } from 'src/config-global';
 import { HomeView } from 'src/sections/home/view';
 import { ISettings } from 'src/types/settings';
-import { endpoints, server_axios } from 'src/utils/axios';
+import { endpoints, server_axios, fetcher } from 'src/utils/axios';
 
 // ----------------------------------------------------------------------
 
@@ -14,7 +15,14 @@ type Props = {
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
   // Fetch product data from server
-  const { data }: { data: ISettings } = await server_axios.get(endpoints.settings.meta);
+  // const { data }: { data: ISettings } = await server_axios.get(endpoints.settings.meta);
+
+  const data = await fetch(BACKEND_API + endpoints.settings.meta, {
+    cache: 'no-store',
+    // next: { revalidate: 60 },
+  }).then((res) => res.json());
+
+  console.log(data);
 
   return {
     title: data?.meta_title || 'جزئیات محصول',
