596e256abb7dee2a3e65cce6db04dbbd9d3f20a5
alireza kargar <alirezakargarsuport@gmail.com>
2025-01-30 20:19:26 +0330

    working on delete

---
+++ src/sections/admin-panel/access-level/access-level-table-row.tsx
@@ -0,0 +1,85 @@
+import { IconButton, Stack, TableCell, TableRow } from "@mui/material";
+import { ConfirmDialog } from "src/components/custom-dialog";
+import Label from "src/components/label";
+import { PrimaryButton } from "src/components/styles/buttons/primary";
+import SvgColor from "src/components/svg-color";
+import { useBoolean } from "src/hooks/use-boolean";
+import { IAdmin } from "src/types/admin";
+import { adminRoleTranslate } from "src/utils/admin-role";
+
+
+interface Props {
+    row: IAdmin;
+    index: number;
+    length: number;
+    onEditRow: VoidFunction;
+    onDeleteRow: VoidFunction;
+}
+
+export function AccessLevelTableRow({
+    row,
+    index,
+    length,
+    onDeleteRow,
+    onEditRow,
+}: Props) {
+    const deleteDialog = useBoolean();
+
+    return (
+        <>
+            <TableRow
+                sx={{
+                    ...((index === length - 1) && {
+                        borderBottom: 'none'
+                    }),
+                }}
+            >
+                <TableCell>{index + 1}</TableCell>
+
+                <TableCell>{row.fullName}</TableCell>
+
+                <TableCell>
+                    <Label variant="filled" color="blue">
+                        {adminRoleTranslate(row.role)}
+                    </Label>
+                </TableCell>
+
+                <TableCell>{row.username}</TableCell>
+                <TableCell>**********</TableCell>
+                <TableCell>{row.phone}</TableCell>
+
+                <TableCell>
+                    <Stack direction={'row'} justifyContent={'flex-end'}>
+                        <IconButton color={'default'} onClick={() => onEditRow()}>
+                            <SvgColor src='/assets/icons/cart/edit.svg' sx={{ width: 16, height: 16 }} />
+                        </IconButton>
+                        <IconButton color={'default'} onClick={() => {
+                            deleteDialog.onTrue();
+                        }}>
+                            <SvgColor src='/assets/icons/cart/trash.svg' sx={{ width: 16, height: 16 }} />
+                        </IconButton>
+                    </Stack>
+                </TableCell>
+
+
+            </TableRow>
+            <ConfirmDialog
+                open={deleteDialog.value}
+                color={'#F3AB28'}
+                onClose={deleteDialog.onFalse}
+                title="اطمینان از افزودن"
+                content={`آیا از حذف «${row?.fullName}» از عنوان «${adminRoleTranslate(`${row?.role}`)}» اطمینان دارید؟`}
+                closeTitle="خیر"
+                action={
+                    <PrimaryButton size="medium" onClick={() => {
+                        deleteDialog.onFalse();
+                        onDeleteRow();
+                    }}>
+                        بله
+                    </PrimaryButton>
+                }
+            />
+        </>
+    )
+
+}
--- src/sections/admin-panel/access-level/view/access-level-view.tsx
+++ src/sections/admin-panel/access-level/view/access-level-view.tsx
@@ -18,10 +18,13 @@ import { AccessLevelNewEditView } from "../access-level-new-edit-view";
 import { AccessLevelListView } from "./access-level-list-view";
 import { useEffect, useState } from "react";
 import { DialogWithButton } from "src/components/custom-dialog";
+import { AccessLevelTableRow } from "../access-level-table-row";
 
 export default function AccessLevelview() {
     const [id, setId] = useState<number>();
     // const [admin, setAdmin] = useState<IAdmin>();
+
+    const { admins } = useGetAdmins();
 
     const adminDialog = useBoolean();
     // const warningDialog = useBoolean();
@@ -44,7 +47,7 @@ export default function AccessLevelview() {
             </DialogWithButton>
 
             <PageTitle icon="/assets/icons/admin-panel/home-01.svg" title="مدیریت دسترسی ها" />
-            <AccessLevelListView
+            {/* <AccessLevelListView
                 onAddAdmin={() => {
                     setId(undefined)
                     adminDialog.onTrue()
@@ -53,7 +56,89 @@ export default function AccessLevelview() {
                     setId(id)
                     adminDialog.onTrue();
                 }}
-            />
+            /> */}
+
+            <Container sx={{ pl: '20px!important', ml: '0px!important' }}>
+                <Box>
+                    <Box sx={{
+                        bgcolor: 'white',
+                        borderRadius: '16px',
+                        border: '1px solid #E0E0E0',
+                        p: '24px',
+                        boxShadow: '2px 2px 8px 0px #0000001A'
+                    }}>
+                        <Stack
+                            direction={'row'}
+                            sx={{
+                                mb: 3,
+                                pb: 2,
+                                borderBottom: (theme) => `solid 1px ${theme.palette.divider}`
+                            }}
+                            justifyContent={"space-between"}
+                        >
+                            <Typography variant="h4" fontFamily={'peyda-bold'}>سطح دسترسی ها</Typography>
+
+                            <SecondaryButton size="small" variant="outlined">
+                                جزئیات بیشتر
+                                <SvgColor src="/assets/icons/arrow/arrow-narrow-left.svg" sx={{ width: 16, height: 16, ml: 1 }} />
+                            </SecondaryButton>
+                        </Stack>
+                        <AccessLevel />
+                    </Box>
+                </Box>
+                <Box sx={{ mt: 3, display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="h5" fontFamily={'peyda-bold'}>لیست ادمین ها</Typography>
+                    <SecondaryButton size="small" variant="outlined" sx={{ py: 0, px: 1, pr: 1.5 }} onClick={() => {
+                        setId(undefined)
+                        adminDialog.onTrue()
+                    }}>
+                        <Iconify icon="eva:plus-fill" width={16} height={16} sx={{ mr: 0.75 }} />
+                        ادمین جدید
+                    </SecondaryButton>
+                </Box>
+                <Box>
+                    <TableContainer sx={{ overflow: 'unset', mt: 2, boxShadow: '2px 2px 8px 0px #0000001A', borderRadius: '12px', }}>
+                        <Scrollbar>
+                            <Table sx={{ minWidth: 960 }}>
+                                <TableHead>
+                                    <TableRow>
+                                        <TableCell width={40} sx={{ borderTopLeftRadius: '12px' }}></TableCell>
+
+                                        <TableCell>نام و نام خانوادگی</TableCell>
+
+                                        <TableCell>سطح دسترسی</TableCell>
+
+                                        <TableCell>نام کاربری</TableCell>
+
+                                        <TableCell>رمز ورود</TableCell>
+
+                                        <TableCell>شماره موبایل</TableCell>
+
+                                        <TableCell sx={{ borderTopRightRadius: '12px' }}></TableCell>
+                                    </TableRow>
+                                </TableHead>
+
+                                <TableBody>
+                                    {admins.map((row, index) => (
+                                        <AccessLevelTableRow
+                                            row={row}
+                                            index={index}
+                                            length={admins.length}
+                                            key={index}
+                                            onEditRow={() => {
+                                                setId(row.id)
+                                                adminDialog.onTrue();
+                                            }}
+                                            onDeleteRow={() => {
+                                            }}
+                                        />
+                                    ))}
+                                </TableBody>
+                            </Table>
+                        </Scrollbar>
+                    </TableContainer>
+                </Box>
+            </Container>
         </Box>
     )
 }
